9fa0924ba740952703cc8a3feca58abbf42bea68
Poleron402 <zhperon@gmail.com>
2024-06-30 23:12:57 -0700

    removed a page I might not need to use

--- js/script.js
+++ js/script.js
@@ -13,11 +13,8 @@ const APICall = async()=>{
         <img src="${data[i].image}">
         <h4>${data[i].name}</h4><p>Species: ${data[i].species}<br>Status: <span id='status${objectCounter}'>${data[i].status}</span></p>
         </div>`
-        if(i%2==0){
-            document.getElementById(`card${objectCounter}`).style.border = '3px solid rgb(82, 178, 201)'
-        }else{
-            document.getElementById(`card${objectCounter}`).style.border = '3px solid rgb(187, 222, 79)'
-        }        
+        i%2==0 ? document.getElementById(`card${objectCounter}`).style.border = '3px solid rgb(82, 178, 201)':document.getElementById(`card${objectCounter}`).style.border = '3px solid rgb(187, 222, 79)'
+               
         let status = document.getElementById(`status${objectCounter}`)
         if(status.textContent == 'Alive'){
             status.style.color = 'green'
@@ -30,8 +27,18 @@ const APICall = async()=>{
     
 }
 APICall()
-
-const APICallByCharacter = async(charName) =>{
+const APICallByCharacter = async(e) =>{
     
+    let charName = document.getElementById('charName').value
+    let response = await fetch(`https://rickandmortyapi.com/api/character/?page=2&name=${charName}`)
+    let data = await response.json()
+    if(data.results){
+        console.log(data.results)
+    }else{
+        e.preventDefault()
+    }
 }
 document.getElementById('btn').addEventListener('click', APICall)
+document.getElementById('submit').addEventListener('click', (event)=>{
+    APICallByCharacter(event)
+})
