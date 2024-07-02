7f8899645f653f04b10172ca21c161e804e70873
Poleron402 <zhperon@gmail.com>
2024-07-01 20:26:39 -0700

    comments added

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ const specificCharacterInfo  =async(id)=>{
     data.status == 'unknown'? mainDiv.innerHTML += '<h3>No one knows if they are alive or not...</h3>': mainDiv.innerHTML += `<h3>They are still ${data.status}</h3>`
     mainDiv.innerHTML += `<h3>Species: ${data.species}<br>Gender: ${data.gender}<br>Origin: ${data.origin.name}<br>Location: ${data.location.name}<h3>`
 }
+//rendering Characters on the page based on the object we receive back from the API. If its by search, it will be smaller, so adjustments were needed
 const renderCharacters = (dataObj)=>{
     // console.log(pageCount)
     let pageComparison = dataObj.info.pages
@@ -51,18 +52,21 @@ const renderCharacters = (dataObj)=>{
     }
     
 }
+//the main API call that renders ALL 800+ characters on the main page with pagination implemented
 const APICall = async()=>{
     pageCount++;
     let request = await fetch(`https://rickandmortyapi.com/api/character/?page=${pageCount}`)
     let data = await request.json()
-    // console.log(data)
     if (data.results){
         renderCharacters(data)
     }
 }
+//We call it on its own since we want all characters shown on page load
 APICall()
+//different call for specific character
 const APICallByCharacter = async(charName)=>{
     pageCount++
+    //form validation, if we simply press search, all characters will load
     if(charName == ''){
         APICall()
     }
@@ -74,6 +78,7 @@ const APICallByCharacter = async(charName)=>{
         alert('There is no such character:(')
     }
 }
+//we dont want the form to submit immediately, so we need to disable event's default behavior
 const APICallByCharacterEventHandler = (e) =>{
     pageCount = 0
     e.preventDefault()
@@ -81,15 +86,17 @@ const APICallByCharacterEventHandler = (e) =>{
     let charName = document.getElementById('charName').value
     charName == ""? APICall():APICallByCharacter(charName)
 }
+//I had to add a handler since I was having trouble loading only the character that a user searches for. If there is 'stuff' in the text
+//input, then we just keep calling that stuff's endpoint
 const APICallHandler = () =>{
     let charName = document.getElementById('charName').value
-    // console.log(document.getElementById('charName').value)
     if (!document.getElementById('charName').value){
         APICall()
     }else{
         APICallByCharacter(charName)
     }
 }
+//event listeners
 document.getElementById('more').addEventListener('click', APICallHandler)
 document.getElementById('submit').addEventListener('click', (event)=>{
     APICallByCharacterEventHandler(event)
