28b508479e79adf3a4ead3c1e07d24b116d42176
Poleron402 <zhperon@gmail.com>
2024-07-01 20:20:02 -0700

    done, next touch ups

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
+//I really tried routing between pages but that did not work well so here's a lengthy JS to rerender all on one screen
+
 let pageCount = 0
 let objectCounter = 0
-
+//Since I am not reusing this method anywhere, it's not broken down and is stand alone.
+const specificCharacterInfo  =async(id)=>{
+    document.getElementById('more').disabled=true;
+    document.getElementById('more').style.visibility = 'hidden'
+    document.getElementById("mainDiv").innerHTML = ""
+    let response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    let data = await response.json()
+    let mainDiv = document.getElementById("individualDiv")
+    mainDiv.style.visibility = 'visible'
+    mainDiv.innerHTML += `<h2>You have queued 
+    <span style="color:rgb(82, 178, 201)">${data.name}</span></h2><br>
+    <img src="${data.image}"><br>`
+    data.status == 'unknown'? mainDiv.innerHTML += '<h3>No one knows if they are alive or not...</h3>': mainDiv.innerHTML += `<h3>They are still ${data.status}</h3>`
+    mainDiv.innerHTML += `<h3>Species: ${data.species}<br>Gender: ${data.gender}<br>Origin: ${data.origin.name}<br>Location: ${data.location.name}<h3>`
+}
 const renderCharacters = (dataObj)=>{
-    console.log(pageCount)
+    // console.log(pageCount)
     let pageComparison = dataObj.info.pages
+    document.getElementById("individualDiv").style.visibility = 'hidden'
     if (pageCount == pageComparison){
         document.getElementById('more').disabled=true;
         document.getElementById('more').style.visibility = 'hidden'
@@ -17,10 +34,12 @@ const renderCharacters = (dataObj)=>{
         objectCounter += 1
         divBody.innerHTML += `<div class="characterCard" id="card${objectCounter}">
         <img src="${data[i].image}">
-        <h4>${data[i].name}</h4><p>Species: ${data[i].species}<br>Status: <span id='status${objectCounter}'>${data[i].status}</span></p>
+        <h4>${data[i].name}</h4><p>Species: ${data[i].species}<br>Status: <span id='status${objectCounter}'>${data[i].status}</span>
+        </p>
+        <button class = "in-card" onclick ="specificCharacterInfo('${data[i].id}')" >MoreInfo</button>
         </div>`
         i%2==0 ? document.getElementById(`card${objectCounter}`).style.border = '3px solid rgb(82, 178, 201)':document.getElementById(`card${objectCounter}`).style.border = '3px solid rgb(187, 222, 79)'
-               
+        
         let status = document.getElementById(`status${objectCounter}`)
         if(status.textContent == 'Alive'){
             status.style.color = 'green'
@@ -63,11 +82,12 @@ const APICallByCharacterEventHandler = (e) =>{
     charName == ""? APICall():APICallByCharacter(charName)
 }
 const APICallHandler = () =>{
-    console.log(document.getElementById('charName').value)
+    let charName = document.getElementById('charName').value
+    // console.log(document.getElementById('charName').value)
     if (!document.getElementById('charName').value){
         APICall()
     }else{
-        APICallByCharacter()
+        APICallByCharacter(charName)
     }
 }
 document.getElementById('more').addEventListener('click', APICallHandler)
